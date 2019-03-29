8342be1e5002b1d024ebd947eb8dd77cf5f3b65f
adithyakeshav <37297741+adithyakeshav@users.noreply.github.com>
2019-03-30 00:14:33 +0530

    Signup Page
    Page to Sign up for this Website.
    
    * Sign Up requires Special Secret Key which will be shared among the Faculties of the college only
    
    * Sign up fails if Secret Key is invalid
    
    * or username may already exist

---
+++ signup.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<?php
+session_start();
+$db = mysqli_connect("localhost","root","", "kvgce");
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="IMAGES/KVG_LOGO.JPG" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel='stylesheet' href="font/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One" rel="stylesheet">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel='stylesheet' href="font/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<style>
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    color : #132853; 
+}
+form {
+    border: 3px solid #f1f1f1;
+}
+
+img.avatar {
+  width: 20%;
+  border-radius: 50%;
+}
+
+</style>
+</head>
+<body style="background-color: lightseagreen">
+    <div style="height:20px"></div>
+    <div class="row">
+        <div class="col-xs-4"></div>
+        
+        <div class="col-xs-4"  style="background-color:white"> 
+            <h1>Sign Up Form</h1>
+            <form method="POST">
+                <br>
+                <div align="center">
+                  <img src="mages/avatar.png" class="avatar">
+                </div>
+
+                <div style="padding:20px">
+                    <br>
+                    <label class="lead">
+                        <b>Username</b>
+                    </label>
+                    <br>
+                    <input type="text" placeholder="Enter Username" name="newname" required  class="form-control">
+                    <br>
+
+                    <label class="lead"><b>Password</b></label> <br>
+                    <input type="password" placeholder="Enter Password" name="newpasswd" required class="form-control"> <br>
+
+                    <label class="lead"><b>Secret Key</b></label> <br>
+                    <input type="password" placeholder="Enter Secret Key" name="secretkey" required class="form-control"> <br>
+
+                    <p class="lead text-danger">
+                        <?php
+                        if(isset($_GET['fail']) )
+                            echo "Unable to Sign Up..!! Username may already Exist";
+                        if(isset($_GET['invalid']))
+                            echo "Invalid Secret Key..!! Try with Valid Secret Key";
+                        ?>
+                    </p>
+
+                    <button type="submit" class="btn btn-primary form-control" name="submit">Register</button> <br> <br>
+                    <a href="login.php" class="btn btn-info form-control" >Already have an account? Sign In</a>
+                </div>
+            </form>
+            <br>
+        </div>
+        <div class="col-xs-4"></div>
+    </div>
+    <?php 
+    if(isset($_POST['submit'])) {
+        
+        $key = mysqli_query($db, "select * from secret_key");
+        $row = mysqli_fetch_assoc($key);
+        if($row['value']== $_POST['secretkey']) {
+        
+            $sql = "insert into login (username,passwd ) values ( '".$_POST["newname"]."','".$_POST["newpasswd"]."');";
+            if(mysqli_query($db,$sql)) {
+                $_SESSION['user'] = $_POST["newname"];
+                header("location:exit.php");
+            } else {
+                header("location:signup.php?fail=true");
+            }
+        } else {
+            header("location:signup.php?invalid=true");
+        }
+    }
+    ?>
+  
+</body>
+</html>
