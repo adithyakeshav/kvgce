1bac76b0c18f14db9f255c8fca6be0d9603710bd
Ajith M S <48750232+ajithms@users.noreply.github.com>
2019-03-30 10:03:01 +0530

    Update-1.1
    * view_exit page added- gives a report on Exit Feedback
    * Slight Modifications in Signup Page
    * "Move to Top" - Button modification
    *

--- signup.php
+++ signup.php
@@ -1,28 +1,23 @@
 <!DOCTYPE html>
 <?php
-session_start();
-$db = mysqli_connect("localhost","root","", "kvgce");
+        session_start();
+     $db = mysqli_connect("localhost","root","", "kvgce");
 ?>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <link rel="shortcut icon" href="IMAGES/KVG_LOGO.JPG" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <link rel='stylesheet' href="font/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One" rel="stylesheet">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel='stylesheet' href="font/css/font-awesome.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+     <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+         <link rel='stylesheet' href="font/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One" rel="stylesheet">
+         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel='stylesheet' href="font/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+           <link rel="shortcut icon" href="images/kvg_logo.jpg"/>
 <style>
-body {
-    font-family: Arial, Helvetica, sans-serif;
-    color : #132853; 
-}
-form {
-    border: 3px solid #f1f1f1;
-}
+body {font-family: Arial, Helvetica, sans-serif;color : #132853; }
+form {border: 3px solid #f1f1f1;}
 
 img.avatar {
   width: 20%;
@@ -32,48 +27,39 @@ img.avatar {
 </style>
 </head>
 <body style="background-color: lightseagreen">
-    <div style="height:20px"></div>
+    <div style="height:30px"></div>
     <div class="row">
         <div class="col-xs-4"></div>
         
         <div class="col-xs-4"  style="background-color:white"> 
-            <h1>Sign Up Form</h1>
-            <form method="POST">
-                <br>
-                <div align="center">
-                  <img src="mages/avatar.png" class="avatar">
-                </div>
+        <h1>Sign Up Form</h1>
+        <form method="POST">
+            <br><div align="center">
+              <img src="images/avatar.png"  class="avatar">
+          </div>
 
-                <div style="padding:20px">
-                    <br>
-                    <label class="lead">
-                        <b>Username</b>
-                    </label>
-                    <br>
-                    <input type="text" placeholder="Enter Username" name="newname" required  class="form-control">
-                    <br>
+            <div style="padding:30px">
+                <br><label class="lead"><b>Username</b></label>
+              <input type="text" placeholder="Enter Username" name="newname" required  class="form-control">
 
-                    <label class="lead"><b>Password</b></label> <br>
-                    <input type="password" placeholder="Enter Password" name="newpasswd" required class="form-control"> <br>
-
-                    <label class="lead"><b>Secret Key</b></label> <br>
-                    <input type="password" placeholder="Enter Secret Key" name="secretkey" required class="form-control"> <br>
-
-                    <p class="lead text-danger">
+              <br>  <label class="lead"><b>Password</b></label>
+              <input type="password" placeholder="Enter Password" name="newpasswd" required class="form-control">
+              <br><label class="lead"><b>Faculty ID</b></label> 
+                    <input type="password" placeholder="Enter ID" name="secretkey" required class="form-control"> 
+                     <p class="lead">
                         <?php
                         if(isset($_GET['fail']) )
-                            echo "Unable to Sign Up..!! Username may already Exist";
+                            echo " Username already Exists";
                         if(isset($_GET['invalid']))
-                            echo "Invalid Secret Key..!! Try with Valid Secret Key";
+                            echo "Invalid Faculty ID!..";
                         ?>
                     </p>
-
-                    <button type="submit" class="btn btn-primary form-control" name="submit">Register</button> <br> <br>
-                    <a href="login.php" class="btn btn-info form-control" >Already have an account? Sign In</a>
-                </div>
-            </form>
-            <br>
-        </div>
+              <button type="submit" class="btn btn-primary form-control" name="submit">Register</button><br>
+               <br><p align="center" class="lead">--- OR ---</p>
+               <a href="login.php" class="btn btn-info form-control" >Already have an account? Sign In</a>
+           </div>
+        </form><br>
+          </div>
         <div class="col-xs-4"></div>
     </div>
     <?php 
@@ -95,6 +81,6 @@ img.avatar {
         }
     }
     ?>
-  
+  <div style="height:30px"></div>
 </body>
 </html>
