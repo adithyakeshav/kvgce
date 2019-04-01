1a3350c31555a16515963749559d4191d04a23a1
adithyakeshav <37297741+adithyakeshav@users.noreply.github.com>
2019-04-01 10:12:30 +0530

    option page added

---
+++ option.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<?php
+    include 'header.php';
+    
+    if(!isset($_SESSION['uname']))
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div class="container">
+            
+        </div>
+    </body>
+</html>
