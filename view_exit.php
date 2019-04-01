3d47f968b007768b372dd549d408f85c7f6d75bb
adithyakeshav <37297741+adithyakeshav@users.noreply.github.com>
2019-04-01 07:07:01 +0530

    Statistics included
    Statistics on how many students Strongly agree, Fairly Agree or Disagree to the given question

--- view_exit.php
+++ view_exit.php
@@ -6,6 +6,16 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="IMAGES/KVG_LOGO.JPG" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+         <link rel='stylesheet' href="font/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One" rel="stylesheet">
+         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel='stylesheet' href="font/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
         <title></title>
         <style>
             table, th, tr, td, caption {
@@ -34,31 +44,48 @@
         </style>
     </head>
     <body>
-        <form method="post" >
-                <table align='center'>
-                <caption><b>Exit_Feedback </b></caption>
-                <th>Name</th>
-                <th>USN</th>
-                <th>Graduation year</th>
-                <th>PO1</th>
-                <th>P02</th>
-                <th>P03</th>
-                <th>P04</th>
-                <th>P05</th>
-                <th>P06</th>
-                <th>P07</th>
-                <th>P08</th>
-                <th>P09</th>
-                <th>P010</th>
-                <th>P011</th>
-                <th>P012</th>
-                </tr>
+        <div class="container">
+            <form method="post">
+            <div class="form form-inline">
+                <input type="number" name="year" minlength="4" maxlength="4"autocomplete="off" list="list1" placeholder="year">
+                <input type="submit" name="submit" class="btn btn-success"> 
+            </div>
+        </form>
+            <?php
+            if(isset($_POST['submit'])) {
+                $year = $_POST['year'];
+                $result = mysqli_query($db, "select * from exit_feedback where year='".$year."';");
+                if(mysqli_num_rows($result) <= 0) {
+                    ?>
+            <p class="lead">No entry for given year <?php echo $year; ?></p>
+            <?php
+                }
+                else
+                {
                 
+            ?>
+        <table align='center'>
+        <caption><b>Exit_Feedback </b></caption>
+        <th>Name</th>
+        <th>USN</th>
+        <th>Graduation year</th>
+        <th>PO1</th>
+        <th>P02</th>
+        <th>P03</th>
+        <th>P04</th>
+        <th>P05</th>
+        <th>P06</th>
+        <th>P07</th>
+        <th>P08</th>
+        <th>P09</th>
+        <th>P010</th>
+        <th>P011</th>
+        <th>P012</th>
+        </tr>
+
 
         <?php
-      	$result=mysqli_query($db,"select * from exit_feedback");
-	if(mysqli_num_rows($result) > 0) {
-	while($row=mysqli_fetch_assoc($result)) { ?>
+        while($row=mysqli_fetch_assoc($result)) { ?>
             <tr>
                <td><?php echo $row['name']; ?></td>
                <td><?php echo $row['usn']; ?></td>
@@ -77,13 +104,70 @@
                 <td><?php echo $row['p12']; ?></td>
 
             </tr>
-            
-            <?php        
-                                                   }
-                }
-                   
-        ?>
+
+            <?php  
+            }
+            ?>
             </table> 
-        </form>
+            <div style="height:100px"></div>
+            <table align="center">
+                <caption>Statistics</caption>
+                <caption style="font-size: 20px">
+                    <?php
+                        $num = mysqli_query($db, "select COUNT(*) from exit_feedback where year='".$year."';");
+                        echo "Total Submissions : ".mysqli_fetch_assoc($num)['COUNT(*)'];
+                    ?>
+                </caption>
+                <tr>
+                    <th>Question</th>
+                    <th>Strongly Agree</th>
+                    <th>Fairly Agree</th>
+                    <th>Disagree</th>
+                </tr>
+                <?php
+                for($i = 1; $i < 13; $i++) {
+                    if($i > 9)
+                        $question = "p".$i;
+                    else
+                        $question = "p0".$i;
+                ?>
+                <tr>
+                    <td>
+                        Question <?php echo $question; ?>
+                    </td>
+                    <td>
+                        <?php
+                            $query = "SELECT COUNT(*) as count FROM exit_feedback WHERE year='".$year."' AND ".$question."='3';";
+                            $p1 = mysqli_query($db, $query);
+                            $p1_strongly = mysqli_fetch_assoc($p1);
+                            echo $p1_strongly['count'];
+                        ?>
+                    </td>
+                    <td>
+                        <?php
+                            $query = "SELECT COUNT(*) as count FROM exit_feedback WHERE year='".$year."' AND ".$question."='2';";
+                            $p1 = mysqli_query($db, $query);
+                            $p1_fairly = mysqli_fetch_assoc($p1);
+                            echo $p1_fairly['count'];
+                        ?>
+                    </td>
+                    <td>
+                        <?php
+                            $query = "SELECT COUNT(*) as count FROM exit_feedback WHERE year='".$year."' AND ".$question."='1';";
+                            $p1 = mysqli_query($db, $query);
+                            $p1_disagree = mysqli_fetch_assoc($p1);
+                            echo $p1_disagree['count'];
+                        ?>
+                    </td>
+                </tr>
+                <?php
+                }
+                ?>
+            </table>
+        </div>
+        <?php
+            }
+            }
+        ?>
     </body>
 </html>
