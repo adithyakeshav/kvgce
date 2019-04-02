c66a851a37c9cf207503250df186811a66808fe5
adithyakeshav <37297741+adithyakeshav@users.noreply.github.com>
2019-04-02 06:57:26 +0530

    Add_subject.php-Dynamic Jumbotrons
    *Dynamic entry of Questions
    *Initial entry will be the number of Questions

--- add_subject.php
+++ add_subject.php
@@ -5,7 +5,6 @@
     if(!isset($_SESSION['user'])) {
         header("location:login.php");
     }
-    
     if(isset($_POST['submit'])) {
         
         //if(isset)
@@ -32,6 +31,11 @@
                            ?>
                            maxlength="40" name='sub_name' placeholder="Subject Name" class="form-control">
             </div>
+            
+            
+            
+            
+            
             <br>
             <div class="form-inline row">
                     <div class="col-xs-6 row" >
@@ -56,9 +60,9 @@
                         <label class="lead col-xs-5">Scheme : </label>
                         
                                 
-                        <select name="year"
+                        <select required
+                                name="year"
                                 class="lead col-xs-7 form-control" >
-                            <option value="#">--select year--</option>
                             <option value="2010">2010</option>
                             <option value="2015">2015</option>
                             <option value="2017">2017</option>
@@ -69,44 +73,56 @@
             <br>
             
             <?php
-            if(isset($_POST['add'])|| isset($_POST['add'])) {
-                
-            }
-                
+            if(!isset($_POST['year'])) {
             ?>
+            
+            <div>
+                <div class="form-inline row">
+                    <label class="lead col-xs-3">How Many Questions?</label>
+                    <input required class="col-xs-6 form-control" type="number" min="1" max="12" placeholder="Num" name="num">
+                </div>
+                <input type="submit" value="Add" class="btn btn-primary col-xs-1" name="add">
+            </div>
+            
+            
+            <?php   } else {
+                $num = $_POST['num'];
+                
+                for($i=1; $i <= $num; $i++) {
+                ?>
+            
             <div class="jumbotron">
-                <div class="row">
+                    <div class="row">
                         <div class="col-xs-9">
-                            <label class="lead">Question :</label>
+                            <label class="lead">Question <?php echo $i; ?> :</label>
                         </div>
                         <div class="col-xs-3">
                             <label class="lead">Criteria :</label>
                         </div>
                     </div>
-                <form method="post">
+                
                     
                     <div class="row">
-                        <textarea required
-                                class="col-xs-9"
-                                  style="height : 80px;"
-                                  name ="question"
-                            placeholder="Question"></textarea>
+                        <div class="col-xs-9">
+                            <textarea required
+                                    class="form-control"
+                                      style="height : 80px;"
+                                      <?php echo " name='question".$i."' "; ?>
+                                placeholder="Question"></textarea>
+                        </div>
                         <div class="col-xs-3">
                             <div>
-                                <label class=>P1 <input type="checkbox" name="p1"></label>
-                                <label class=>P2 <input type="checkbox" name="p2"></label>
-                                <label class=>P3 <input type="checkbox" name="p3"></label>
-                                <label class=>P4 <input type="checkbox" name="p4"></label>
-                                <label class=>P5 <input type="checkbox" name="p5"></label>
+                                <select required name="criteria" class="form-control">
+                                    <?php for($j=1; $j<=$num; $j++) { ?>
+                                    <option><?php echo "po".$j  ?></option>
+                                    <?php   }   ?>
+                                </select>
                             </div>
                         </div>
                     </div>
-                    <br>
-                   <input type="submit"
-                name ="add"
-                value="Add Question"
-                class="btn btn-info">
             </div> 
+                <?php } ?>
+            
              
                 
                 <input type="submit"
@@ -114,10 +130,11 @@
                 value="Submit"
                 class="btn btn-success">
                 
+            <?php  }  ?>
             </div>
             
         </div>
         <div style="height:50px;"></div>
-        </form>
+    </form>
     </body>
 </html>
