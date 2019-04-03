5f31f12e68715bdb3c285782584f2e6dc3e92a97
adithyakeshav <37297741+adithyakeshav@users.noreply.github.com>
2019-04-03 15:34:06 +0530

    cb_feedback update

--- cb_feedback.php
+++ cb_feedback.php
@@ -6,8 +6,25 @@
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="container">
+            <form method="post">
+                <div class="row">
+                    <div class="col-md-6 form-inline">
+                        <p class="lead col-md-6">Name</p>
+                        <input required
+                               autocomplete="off"
+                               type="text"
+                               placeholder=""
+                            
+                            name="name">
+                    </div>
+                    <div class="col-md-6 form-inline">
+                        
+                    </div>
+                    
+                </div>
+                
+            </form>
+        </div>
     </body>
 </html>
